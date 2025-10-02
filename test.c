c905831303468f83a95c52ffe4bbaa215f86881c
creation-man <1853595374@qq.com>
2025-10-02 22:25:15 +0800

    11
    111

---
+++ test.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+
+
+
+int test() {
+    printf("This is a test.\n");
+
+    return 0;
+}
+int main() {
+    printf("Hello World!\n");
+    printf("This is a test.\n");
+
+    return 0;
+
+}
