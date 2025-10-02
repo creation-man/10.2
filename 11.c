c6d6d4b2093cbfd149fecc219e55b1f0faf56c10
creation-man <1853595374@qq.com>
2025-10-02 22:31:36 +0800

    11111
    111111

--- 11.c
+++ 11.c
@@ -3,4 +3,9 @@ int main()
 {
 int a,b,c;
 printf("Enter three numbers:");
+scanf("%d%d%d",&a,&b,&c);
+if(a>=b&&a>=c)
+printf("%d is the largest number.",a);
+else if(b>=a&&b>=c)
+printf("%d is the largest number.",b);
 }
